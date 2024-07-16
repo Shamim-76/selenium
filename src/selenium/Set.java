867eeedb3ad2ab446fe534e3a4b22a21ffbdb098
Shamimtuba <mohd.shamim.teleysia@gmail.com>
2024-07-16 20:58:23 +0530

    PRIMARY JAVA CODE

---
+++ src/selenium/Set.java
@@ -0,0 +1,40 @@
+package selenium;
+
+public class Set {
+
+	public static  void  main (String[] arg) {
+		
+            int a =10;
+            int b=30;
+            if (a<6); {
+            	System.out.println("i love this code");
+             if(b>35);{System.out.println("i love this not code");}
+            
+            }
+            
+            {System.out.println("i love this rsju");}
+            
+           
+            
+            
+       
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/selenium/barcoad.java
@@ -0,0 +1,11 @@
+package selenium;
+
+public class barcoad {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my first java program");
+
+	}
+
+}
